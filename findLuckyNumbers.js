cbe18e222cf13c43a23499f3c6a5a82790d0b185
Lily Springer <lilyspringer@hotmail.com>
2023-02-04 19:38:02 -0700

    challenge complete

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,14 @@
 // Write your code below this line.
+
+function luckyNumbers(n) {
+    let nums = [1,2,3,4,5,6,7,8,9,10]
+    let luckyNums = []
+    for (let i = 0; i < n; i++) {
+        let x = Math.floor(Math.random() * nums.length)
+        luckyNums.push(nums[x])
+        nums.splice(x, 1)
+    }   
+    return luckyNums
+}
+
+console.log(luckyNumbers(3))
